186ad2043e4e9e4d2f672cb543c9efd3aa9af664
Yasenbaka <1260352143@qq.com>
2024-06-19 21:46:45 +0800

    fix：20240619
    调整Main组件

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,55 @@
-<template>
-  <div class="main_container">
-    <div class="main_container_left">
-      <SideMenu/>
-    </div>
-    <div class="main_container_right">
-      <div class="main_container_bg_box">
-        <img class="main_bg" id="main_bg_arona" src="@/assets/arona.jpg" alt="arona"/>
-        <img class="main_bg" id="main_bg_purana" src="@/assets/purana.jpg" alt="purana"/>
-      </div>
-      <Header/>
-<!--      <div style="width: 100%; height: 50px; background-color: #e0d2ff"></div>-->
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
-import  Header from "@/components/Header/Header.vue";
-import SideMenu from "@/components/SideMenu/SideMenu.vue";
+
+import Main from "@/pages/Main.vue";
 </script>
 
-<style scoped lang="sass">
-.main_container
-  display: grid
-  grid-template-columns: 15% 85%
-  .main_container_left
-    border-right: 1px solid #949494
-  .main_container_right
-    position: relative
-    #main_bg_arona
-      position: fixed
-      left: 15%
-    #main_bg_purana
-      position: fixed
-      right: 0
-    .main_bg
-      height: 100vh
-      z-index: -1
-</style>
+<template>
+  <Main/>
+</template>
+
+
+
+<!--<template>-->
+<!--  <div class="main_container">-->
+<!--    <div class="main_container_left">-->
+<!--      <SideMenu/>-->
+<!--    </div>-->
+<!--    <div class="main_container_right">-->
+<!--      <div class="main_container_bg_box">-->
+<!--        <img class="main_bg" id="main_bg_arona" src="@/assets/arona.jpg" alt="arona"/>-->
+<!--        <img class="main_bg" id="main_bg_purana" src="@/assets/purana.jpg" alt="purana"/>-->
+<!--      </div>-->
+<!--      <Header/>-->
+<!--&lt;!&ndash;      <div style="width: 100%; height: 50px; background-color: #e0d2ff"></div>&ndash;&gt;-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script setup lang="ts">-->
+<!--import  Header from "@/components/Header/Header.vue";-->
+<!--import SideMenu from "@/components/SideMenu/SideMenu.vue";-->
+<!--</script>-->
+
+<!--<style scoped lang="sass">-->
+<!--.main_container-->
+<!--  display: grid-->
+<!--  grid-template-columns: 15% 85%-->
+<!--  .main_container_left-->
+<!--    border-right: 1px solid #949494-->
+<!--  .main_container_right-->
+<!--    position: relative-->
+<!--    #main_bg_arona-->
+<!--      position: fixed-->
+<!--      left: 15%-->
+<!--    #main_bg_purana-->
+<!--      position: fixed-->
+<!--      right: 0-->
+<!--    .main_bg-->
+<!--      height: 100vh-->
+<!--      z-index: -1-->
+<!--</style>-->
+
+
 <!--<script setup lang="ts">-->
 <!--import { RouterLink, RouterView } from 'vue-router'-->
 <!--import HelloWorld from '@/components/HelloWorld.vue'-->
--- src/components/SideMenu/SideMenu.vue
+++ src/components/SideMenu/SideMenu.vue
@@ -9,11 +9,30 @@ import SideMenuUserCard from "@/components/SideMenu/SideMenuUserCard/SideMenuUse
   <SideMenuUserCard/>
   <hr style="width: 70%; border-top: 1px solid #88d9ff;"/>
   <ul class="sidemenu_list_box">
-    <li><a href="#">通信联络</a></li>
-    <li><a href="#">喵羽邮箱</a></li>
-    <li><a href="#">奶果功能</a></li>
-    <li><a href="#">奶果功能</a></li>
-    <li><a href="#">奶果功能</a></li>
+    <li><a href="#">
+      <div>奶果频道信</div>
+      <div style="font-size: 13px">NaigosTalk</div>
+    </a></li>
+    <li><a href="#">
+      <div>喵羽邮箱</div>
+      <div style="font-size: 13px">YuMail</div>
+    </a></li>
+    <li><a href="#">
+      <div>奶果轻娱乐</div>
+      <div style="font-size: 13px">NaigosFunction</div>
+    </a></li>
+    <li><a href="#">
+      <div>共同学习</div>
+      <div style="font-size: 13px">StudyTogether</div>
+    </a></li>
+    <li><a href="#">
+      <div>站长博客</div>
+      <div style="font-size: 13px">Yasenbaka'sBlog</div>
+    </a></li>
+    <li><a href="#">
+      <div style="font-size: 15px">哈尔滨喵羽科技有限公司</div>
+      <div style="font-size: 13px">Miaoyu Technology Co.,Ltd</div>
+    </a></li>
   </ul>
 </div>
 </template>
@@ -30,11 +49,14 @@ import SideMenuUserCard from "@/components/SideMenu/SideMenuUserCard/SideMenuUse
     li
       list-style: none
       margin-bottom: 10%
-    a
       border: 1px solid #ffacef
       border-radius: 10px
-      text-align: center
       height: 40px
+      text-align: center
+      transition: .5s
+    li:hover
+      background-color: #ffacef30
+    a
       text-decoration: none
       color: #ffacef
 </style>
