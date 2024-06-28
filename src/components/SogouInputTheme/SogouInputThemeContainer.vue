c700b9e4df5df0652d70c823a4072d157f82ab67
Yasenbaka <1260352143@qq.com>
2024-06-28 20:12:21 +0800

    fix：修改SogouInputTheme界面

--- src/components/SogouInputTheme/SogouInputThemeContainer.vue
+++ src/components/SogouInputTheme/SogouInputThemeContainer.vue
@@ -8,23 +8,52 @@
     <div class="list_item">星空列车与白的旅行</div>
     <div class="list_item">蔚蓝档案</div>
     <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
-    <div class="list_item">绫月乃萝</div>
+    <div class="list_item">暂未开放</div>
+    <div class="list_item">暂未开放</div>
+    <div class="list_item">暂未开放</div>
+    <div class="list_item">暂未开放</div>
+    <div class="list_item">暂未开放</div>
+    <div class="list_item">暂未开放</div>
+    <div class="list_item">暂未开放</div>
   </div>
-  <div class="route_view">路由区</div>
+  <div>
+    <input class="search_bar" placeholder="若分类无法找到，可以搜索关键词，Enter键结束"/>
+    <div class="route_view">
+      <div class="detail_list">
+        <div class="detail_list_item">Saiba Momoi</div>
+        <div class="detail_list_item">Saiba Midori</div>
+        <div class="detail_list_item">Tentou Alis</div>
+        <div class="detail_list_item">Hanago Yuzu</div>
+        <div class="detail_list_item">Saiba Momoi</div>
+        <div class="detail_list_item">Saiba Midori</div>
+        <div class="detail_list_item">Tentou Alis</div>
+        <div class="detail_list_item">Hanago Yuzu</div>
+        <div class="detail_list_item">Saiba Momoi</div>
+        <div class="detail_list_item">Saiba Midori</div>
+        <div class="detail_list_item">Tentou Alis</div>
+        <div class="detail_list_item">Hanago Yuzu</div>
+      </div>
+      <div>路由视图区</div>
+    </div>
+    <div class="download_bar">
+      下载信息
+    </div>
+  </div>
 </div>
 </template>
 
 <style scoped lang="sass">
+@keyframes container_appear
+  0%
+    opacity: 0
+  100%
+    opacity: 1
+@mixin container_appear_animation
+  animation: container_appear 1s 3s ease
+  animation-fill-mode: forwards
 .container
-  //height: 800px
+  @include container_appear_animation()
+  opacity: 0
   margin: 50px 3%
   display: grid
   grid-template-columns: 30% 69%
@@ -45,7 +74,26 @@
       height: 20%
       width: 45%
       border: 1px solid #33354c
+  .search_bar
+    background-color: #ffffff00
+    width: 30%
+    height: 4%
+    margin: 0 0 1% auto
+  .download_bar
+    margin: 1% auto 0 auto
+    height: 20%
+    border: 1px solid black
   .route_view
     border: 1px solid black
     height: 500px
+    display: grid
+    grid-template-columns: 20% 79%
+    grid-gap: 1%
+    grid-template-rows: 500px
+    .detail_list
+      overflow-x: auto
+      .detail_list_item
+        height: 10%
+        border: 1px solid black
+
 </style>
