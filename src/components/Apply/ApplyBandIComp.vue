7ca354ffcced3d77a0b9ec955ce46d8a2087b2c0
Yasenbaka <1260352143@qq.com>
2024-10-21 15:10:45 +0800

    加入奶果的路由配置

---
+++ src/components/Apply/ApplyBandIComp.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import ApplyBannerComp from "@/components/Apply/ApplyBannerComp.vue";
+import ApplyItemComp from "@/components/Apply/ApplyItemComp.vue";
+</script>
+
+<template>
+  <ApplyBannerComp/>
+  <ApplyItemComp/>
+</template>
+
+<style scoped lang="sass">
+
+</style>
--- src/router/RouterList/ApplyRouter.ts
+++ src/router/RouterList/ApplyRouter.ts
@@ -1,7 +1,14 @@
 import ApplyView from "@/views/Apply/ApplyView.vue";
+import ApplyBandIComp from "@/components/Apply/ApplyBandIComp.vue";
 
 export default {
     path: '/apply',
-    name: 'Apply',
-    component: ApplyView
+    component: ApplyView,
+    children: [
+        {
+            path: '',
+            name: 'Apply',
+            component: ApplyBandIComp
+        }
+    ]
 }
