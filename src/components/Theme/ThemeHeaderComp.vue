210d664ecc6e66b50f50b5b033169b5bf35db4bd
Yasenbaka <1260352143@qq.com>
2024-11-18 21:42:08 +0800

    美化主题界面开发

---
+++ src/components/Theme/ThemeHeaderComp.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import defaultAvatar from "@/assets/Main/avatar.jpg";
+import {useUserDetailStore} from "@/stores/User/UserDetailStore";
+import {ref, watch} from "vue";
+const userDetailStore = useUserDetailStore();
+
+const avatar = ref<string | null>(userDetailStore.userAvatar);
+
+watch(() => userDetailStore.userAvatar, newValue => {
+  avatar.value = newValue;
+});
+</script>
+
+<template>
+  <header class="header">
+    <img class="avatar" :src="avatar || defaultAvatar" alt="avatar"/>
+    <img class="header_bg" src="@/assets/Apply/button_title_bg.svg" alt="bg"/>
+    <span class="title">欢迎访问美化包主题！</span>
+  </header>
+</template>
+
+<style scoped lang="sass">
+.header
+  width: 100%
+  height: 60px
+  background-color: #f0d4eb
+  margin: 0 auto
+  display: flex
+  align-items: center
+  position: relative
+  .header_bg
+    position: absolute
+    width: 100%
+  .avatar
+    margin-left: 20px
+    height: 80%
+    border-radius: 50%
+    border: #bc9bb6 1px solid
+    z-index: 1
+  .title
+    margin: 0 20px 0 auto
+    color: white
+    font-weight: bold
+    font-size: 18px
+    letter-spacing: 1px
+    z-index: 1
+</style>
--- src/views/ThemeView.vue
+++ src/views/ThemeView.vue
@@ -1,13 +1,42 @@
 <script setup lang="ts">
+import banner1 from "@/assets/Theme/banner1.jpg";
+import banner2 from "@/assets/Theme/banner2.jpg";
+import ThemeHeaderComp from "@/components/Theme/ThemeHeaderComp.vue";
 
+const themeClassifyList: Array<{title: string; routeName: string; cover_image: string}> = [
+  {title: '战舰世界', routeName: 'ThemeWows', cover_image: ''},
+]
+const bannerList: Array<any> = [banner1, banner2];
 </script>
 
 <template>
-<div>
-  正在开发中
-</div>
+  <ThemeHeaderComp/>
+  <div class="container">
+    <div class="block text-center">
+      <el-carousel height="300px">
+        <el-carousel-item v-for="item in bannerList" :key="item">
+          <img :src="item" alt="banner"/>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="item_box">
+      <div class="item" v-for="(item, index) in themeClassifyList" :key="index">{{item.title}}</div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="sass">
-
+.container
+  .text-center
+    box-shadow: #57505d50 0 3px 3px
+.item_box
+  width: 90%
+  margin: 50px auto 0 auto
+  display: flex
+  .item
+    border-radius: 10px
+    width: 300px
+    height: 400px
+    border: #57505d solid 1px
+    background-color: #f0d4eb50
 </style>
